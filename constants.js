0e1ead1e40d284d45dc30f831e8fd593e80e9c80
AhmerAftabPatel <ahmerpatel123@gmail.com>
2022-12-02 21:25:25 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   // API: 'https://rf-school.herokuapp.com/api/v1',
-  API : 'rf-school-backend-kglqok11b-ahmeraftabpatel.vercel.app',
+  API : 'https://rf-school-backend.vercel.app/api/v1',
   // API: 'http://localhost:8000/api/v1',
   s3_url : "https://rfschool.s3.ap-south-1.amazonaws.com/"
 };
