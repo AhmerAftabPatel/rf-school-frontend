64841006442d1a436419b255330d579da89d7805
AhmerAftabPatel <ahmerpatel123@gmail.com>
2022-12-02 21:23:20 +0530

    .

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
 module.exports = {
-  API: 'https://rf-school.herokuapp.com/api/v1',
+  // API: 'https://rf-school.herokuapp.com/api/v1',
+  API : 'rf-school-backend-kglqok11b-ahmeraftabpatel.vercel.app',
   // API: 'http://localhost:8000/api/v1',
   s3_url : "https://rfschool.s3.ap-south-1.amazonaws.com/"
 };
