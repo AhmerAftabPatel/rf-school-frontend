9e1bac7a0b89571117f57ca3c4a1bd849630958e
AhmerAftabPatel <ahmerpatel123@gmail.com>
2022-12-14 13:19:26 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,35 @@ import { LinkProvider } from '../context/links/auth/LinkContext';
 import { setup } from 'twind';
 import twindConfig from '../twind.config';
 import Page from '@/components/page';
-
+import { useEffect, useState } from 'react';
+import Router from 'next/router';
 if (typeof window !== `undefined`) {
   setup(twindConfig);
 }
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const start = () => {
+      console.log('start');
+      setLoading(true);
+    };
+    const end = () => {
+      console.log('finished');
+      setLoading(false);
+    };
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
   return (
     // <LinkProvider>
-      <Page>
-        <Component {...pageProps} />
-      </Page>
+    <Page>{loading ? <h3 style={{ textAlign: 'center' }}>Loading...</h3> : <Component {...pageProps} />}</Page>
     // </LinkProvider>
   );
 }
